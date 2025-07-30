8e5ed8fe139eec077139477717c04c33f31cc598
Rudi Farkas <rudi.farkas@gmail.com>
2025-07-31 00:39:26 +0200

    add updated lang files

--- public/i18n/translations/index.js
+++ public/i18n/translations/index.js
@@ -1,5 +1,7 @@
-export { en } from "./en";
 export { fr } from "./fr";
 export { de } from "./de";
+export { it } from "./it";
+export { rm } from "./rm";
+export { en } from "./en";
 export { ja } from "./ja";
 export { zg } from "./zg";
